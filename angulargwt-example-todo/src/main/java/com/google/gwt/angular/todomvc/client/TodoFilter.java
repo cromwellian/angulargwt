f89912257a43ddd1444f3cc33c50315e15c3c126
h0ru5 <johannes.hund@gmail.com>
2014-01-01 19:36:41 +0100

    added workaround for boxing problem

--- angulargwt-example-todo/src/main/java/com/google/gwt/angular/todomvc/client/TodoFilter.java
+++ angulargwt-example-todo/src/main/java/com/google/gwt/angular/todomvc/client/TodoFilter.java
@@ -2,7 +2,9 @@ package com.google.gwt.angular.todomvc.client;
 
 import com.google.gwt.angular.client.Filter;
 import com.google.gwt.angular.client.NgInject;
+
 import elemental.js.util.JsArrayOf;
+import elemental.json.JsonValue;
 import elemental.util.ArrayOf;
 
 /**
@@ -10,13 +12,14 @@ import elemental.util.ArrayOf;
  */
 @NgInject(name = "todoFilter")
 public class TodoFilter implements Filter<Todo> {
-  public ArrayOf<Todo> filter(ArrayOf<Todo> todos, Todo todoFilter) {
+  	
+	public ArrayOf<Todo> filter(ArrayOf<Todo> todos, Todo todoFilter) {
     ArrayOf<Todo> result = JsArrayOf.create();
     for (int i = 0; i < todos.length(); i++) {
       Todo todo = todos.get(i);
       boolean allMatch = true;
       for (String key : todoFilter.json().keys()) {
-        allMatch = allMatch && todo.json().get(key) == todoFilter.json().get(key);
+        allMatch = allMatch && areEqual(todo.json().get(key),todoFilter.json().get(key));
       }
       if (allMatch) {
         result.push(todo);
@@ -24,4 +27,11 @@ public class TodoFilter implements Filter<Todo> {
     }
     return result;
   }
+
+private boolean areEqual(JsonValue val, JsonValue reference) {
+	//FIXME this is a workaround which might result in decreased performance
+	// Elemental boxing disturbes comparision as Object(false)!=false
+	boolean res = (reference.toJson().equals(val.toJson()));
+	return res;
+}
 }
