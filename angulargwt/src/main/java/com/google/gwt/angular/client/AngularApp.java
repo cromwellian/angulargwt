fa31821892b91e57c074d02562881b8cca424904
h0ru5 <johannes.hund@gmail.com>
2014-01-03 19:16:29 +0100

    attempt to lazy-load angularjs if not loaded already

--- angulargwt/src/main/java/com/google/gwt/angular/client/AngularApp.java
+++ angulargwt/src/main/java/com/google/gwt/angular/client/AngularApp.java
@@ -1,7 +1,11 @@
 package com.google.gwt.angular.client;
 
 import com.google.gwt.angular.client.impl.AngularModuleBase;
+import com.google.gwt.core.client.Callback;
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.ScriptInjector;
+
+import elemental.client.Browser;
 import elemental.js.util.JsArrayOfString;
 
 /**
@@ -9,24 +13,63 @@ import elemental.js.util.JsArrayOfString;
  */
 public abstract class AngularApp implements EntryPoint {
 
-  /**
-   * This is the entry point method.
-   */
-  public final void onModuleLoad() {
-    final AngularModule[] modules = main();
-    final JsArrayOfString moduleNames = JsArrayOfString.create();
-    for (int i = 0; i < modules.length; i++) {
-      moduleNames.push(((AngularModuleBase) modules[i]).moduleName());
-    }
-    bootstrap(moduleNames);
-  }
+	private static final String ANGULAR_MIN_JS_URL = "http://ajax.googleapis.com/ajax/libs/angularjs/1.2.1/angular.min.js";
 
-  /**
-   * Override this and invoke GWT.create() on your modules.
-   */
-  protected abstract AngularModule[] main();
+	/**
+	 * This is the entry point method.
+	 */
+	public final void onModuleLoad() {
+		loadAngular();
+	}
 
-  private native void bootstrap(JsArrayOfString moduleNames) /*-{
-      $wnd.angular.bootstrap($doc, moduleNames);
-  }-*/;
+	/**
+	 * Override this and invoke GWT.create() on your modules.
+	 */
+	protected abstract AngularModule[] main();
+
+	private void loadAngular() {
+		if(!isInjected()) {
+		ScriptInjector
+				.fromUrl(
+						ANGULAR_MIN_JS_URL)
+				.setCallback(new Callback<Void, Exception>() {
+					@Override
+					public void onFailure(Exception reason) {
+						Browser.getWindow().getConsole()
+								.warn("loading angularjs failed.");
+					}
+
+					@Override
+					public void onSuccess(Void result) {
+						Browser.getWindow().getConsole()
+								.info("loading angularjs succeeded.");
+						injectModules();
+					}
+				}).setWindow(ScriptInjector.TOP_WINDOW).inject();
+		} else {
+			Browser.getWindow().getConsole()
+			.info("angularjs already loaded.");
+			injectModules();
+		}
+	}
+
+	private void injectModules() {
+		final JsArrayOfString moduleNames = JsArrayOfString.create();
+		final AngularModule[] modules = main();
+		for (int i = 0; i < modules.length; i++) {
+			moduleNames.push(((AngularModuleBase) modules[i]).moduleName());
+		}
+		bootstrap(moduleNames);
+	}
+
+	private native final boolean isInjected() /*-{
+	    if (!(typeof $wnd.angular === "undefined") && !(null===$wnd.angular)) {
+	        return true;
+	    }
+	    	return false;
+	}-*/;
+	
+	private native void bootstrap(JsArrayOfString moduleNames)	/*-{
+			$wnd.angular.bootstrap($doc, moduleNames);
+	}-*/;
 }
