6956ec8bc08b1ec6e47805d20f2986c58b01fb16
h0ru5 <johannes.hund@gmail.com>
2014-02-07 13:28:51 +0100

    added basic archetype for a module

---
+++ angulargwt-module-archetype/src/main/resources/archetype-resources/src/test/java/__moduleName__ModuleTest.java
@@ -0,0 +1,12 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ${moduleName}ModuleTest {
+
+}
