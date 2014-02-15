8edc6c4a41eca2416e8de167b0924ad1d5bb8fa3
h0ru5 <johannes.hund@gmail.com>
2014-02-16 00:28:24 +0100

    introduced gwtquery for promises, fixes #3

--- angulargwt/src/main/java/com/google/gwt/angular/client/AngularApp.java
+++ angulargwt/src/main/java/com/google/gwt/angular/client/AngularApp.java
@@ -4,6 +4,10 @@ import com.google.gwt.angular.client.impl.AngularModuleBase;
 import com.google.gwt.core.client.Callback;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.ScriptInjector;
+import com.google.gwt.query.client.Function;
+import com.google.gwt.query.client.Promise;
+import com.google.gwt.query.client.Promise.Deferred;
+import com.google.gwt.query.client.plugins.deferred.PromiseFunction;
 
 import elemental.client.Browser;
 import elemental.js.util.JsArrayOfString;
@@ -16,41 +20,61 @@ public abstract class AngularApp implements EntryPoint {
 	private static final String ANGULAR_MIN_JS_URL = "http://ajax.googleapis.com/ajax/libs/angularjs/1.2.1/angular.min.js";
 
 	/**
-	 * This is the entry point method.
-	 */
-	public final void onModuleLoad() {
-		loadAngular();
-	}
-
-	/**
 	 * Override this and invoke GWT.create() on your modules.
 	 */
 	protected abstract AngularModule[] main();
 
+	private final static class ScriptLoader extends PromiseFunction {
+
+		@Override
+		public void f(final Deferred dfd) {
+			ScriptInjector
+			.fromUrl(ANGULAR_MIN_JS_URL)
+			.setCallback(new Callback<Void, Exception>() {
+				@Override
+				public void onFailure(Exception reason) {
+					dfd.reject(reason);
+				}
+
+				@Override
+				public void onSuccess(Void result) {
+					dfd.resolve();
+				}
+			}).setWindow(ScriptInjector.TOP_WINDOW).inject();				
+		}
+
+
+	}
+
+
 	private void loadAngular() {
 		if(!isInjected()) {
-		ScriptInjector
-				.fromUrl(
-						ANGULAR_MIN_JS_URL)
-				.setCallback(new Callback<Void, Exception>() {
-					@Override
-					public void onFailure(Exception reason) {
-						Browser.getWindow().getConsole()
-								.warn("loading angularjs failed.");
-					}
+			Promise loadAngular = new ScriptLoader();
 
-					@Override
-					public void onSuccess(Void result) {
-						Browser.getWindow().getConsole()
-								.info("loading angularjs succeeded.");
-						injectModules();
-					}
-				}).setWindow(ScriptInjector.TOP_WINDOW).inject();
+			loadAngular.done(new Function() {
+				public void f() {
+					Browser.getWindow().getConsole().info("loading angularjs succeeded.");
+					injectModules();					
+				}
+			});
+
+			loadAngular.fail(new Function() {
+				public void f() {
+					Browser.getWindow().getConsole().warn("loading angularjs failed.");
+				}
+			});
+
 		} else {
-			Browser.getWindow().getConsole()
-			.info("angularjs already loaded.");
+			Browser.getWindow().getConsole().info("angularjs already loaded.");
 			injectModules();
 		}
+	}
+
+	/**
+	 * This is the entry point method.
+	 */
+	public final void onModuleLoad() {
+		loadAngular();
 	}
 
 	private void injectModules() {
@@ -68,8 +92,9 @@ public abstract class AngularApp implements EntryPoint {
 	    }
 	    	return false;
 	}-*/;
-	
+
 	private native void bootstrap(JsArrayOfString moduleNames)	/*-{
 			$wnd.angular.bootstrap($doc, moduleNames);
 	}-*/;
 }
+
