43c25e6b05ebd57b8869375256486863fc4d7ea3
h0ru5 <johannes.hund@gmail.com>
2014-02-23 18:46:36 +0100

    added more example code to the archetype

--- angulargwt-app-archetype/src/main/resources/archetype-resources/src/main/java/__appName__Controller.java
+++ angulargwt-app-archetype/src/main/resources/archetype-resources/src/main/java/__appName__Controller.java
@@ -4,12 +4,17 @@
 package ${package};
 
 import com.google.gwt.angular.client.AngularController;
+import com.google.gwt.angular.client.NgInject;
+import com.google.gwt.angular.client.NgWatch;
+
+import elemental.client.Browser;
 
 /**
  * This is your Controller
  * append injected services to the onInit-Method
  * all public methods are exposed to the View
  */
+@NgInject(name="${appName}Controller")
 public class ${appName}Controller extends AngularController<${appName}Scope> {
 	public void onInit(${appName}Scope scope) {
 		scope.name("World");
@@ -25,5 +30,10 @@ public class ${appName}Controller extends AngularController<${appName}Scope> {
 	
 	public void toLower() {
 		scope.name(scope.name().toLowerCase());
+	}
+	
+	@NgWatch("name")
+	public void watchName(String newName){
+		Browser.getWindow().getConsole().log("Name changed to " + newName);
 	}
 }
