56136f66096312cd7ef73f8c7bb0a2c79044fd8c
Worathiti Manosroi <w.manosroi@gmail.com>
2014-04-28 16:47:12 +0200

    templateUrl & transclude

--- angulargwt/src/main/java/com/google/gwt/angular/rebind/DirectiveGenerator.java
+++ angulargwt/src/main/java/com/google/gwt/angular/rebind/DirectiveGenerator.java
@@ -1,10 +1,7 @@
 package com.google.gwt.angular.rebind;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
